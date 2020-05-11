ac8f078182c533ba453582b298cc5fbe1e958213
Sergio Alvarez <sergio@codefit.io>
2020-05-11 20:37:19 +1000

    Upgrade mocha

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 exports.host = process.env['WERCKER_RETHINKDB_HOST'] || 'localhost';
 exports.port = process.env['WERCKER_RETHINKDB_PORT'] || 28015;
 exports.db = 'thinky_test';
-exports.authKey = '';
+exports.user = 'admin';
+exports.password = 'admin';
